87314b9259f928fca870c878ab4052d647041834
xavivi8 <jmartinlunasescobar@gmail.com>
2024-02-19 17:21:26 +0100

    Main Servidor CHANGES

--- Servidor/src/main/java/servidor/MainServidor.java
+++ Servidor/src/main/java/servidor/MainServidor.java
@@ -4,7 +4,17 @@ public class MainServidor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Conexion conexion = new Conexion();
+		
+		try {
 
+			conexion.conexion();
+
+		} catch (Exception e) {
+
+			System.out.println(e);
+			// TODO: handle exception
+		}
 	}
 
 }
