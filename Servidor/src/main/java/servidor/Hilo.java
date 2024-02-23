79aab3ad03bcf4b9beca7cdf60a0d2e41b69f029
xavivi8 <jmartinlunasescobar@gmail.com>
2024-02-23 17:44:47 +0100

    Hilo.java CHANGES

--- Servidor/src/main/java/servidor/Hilo.java
+++ Servidor/src/main/java/servidor/Hilo.java
@@ -14,16 +14,17 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Hilo extends Thread{
+public class Hilo extends Thread {
 	private static Configuration config = new Configuration();
-	private static SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	private static SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+			.buildSessionFactory();
 	private static Servicio servicio = new Servicio();
 	private Socket clienteSocket;
-	
+
 	public Hilo(Socket socket) {
 		this.clienteSocket = socket;
 	}
-	
+
 	public void run() {
 		int rolUsuario = -1;
 		/**
@@ -32,51 +33,72 @@ public class Hilo extends Thread{
 		boolean logeado = false;
 		do {
 			String input = "awdwaw,efesf";
-	        String[] result = splitByComma(input);
-			rolUsuario = servicio.obtenerRolUsuario(result[0],result[1]);
-			if(rolUsuario >= 0) {
+			String[] result = splitByComma(input);
+			rolUsuario = servicio.obtenerRolUsuario(result[0], result[1]);
+			if (rolUsuario >= 0) {
 				logeado = true;
 			}
-		}while(logeado == false);
-		
+		} while (logeado == false);
+
 		/**
 		 * Control de roll
 		 */
 		int opcion = -1;
-		if(rolUsuario == 1) {
-			do {
-				System.out.println(Funciones.menuAdmin());
-			}while(opcion != 0);
-		} else {
-			do {
-				
-			}while(opcion != 0);
+		try {
+			if (rolUsuario == 1) {
+				do {
+					enviarMensajeCliente(Funciones.menuAdmin());
+					String vuelta = getClientNameFromSocket();
+					opcion = Integer.parseInt(vuelta);
+					switch (opcion) {
+					case 0:
+						enviarMensajeCliente("Tenga un buen día");
+						break;
+					case 1:
+						System.out.println("Opción 1 seleccionada");
+						break;
+					case 2:
+						System.out.println("Opción 2 seleccionada");
+						break;
+					case 3:
+						System.out.println("Opción 3 seleccionada");
+						break;
+					default:
+						enviarMensajeCliente("Opción no válida");
+						break;
+					}
+				} while (opcion != 0);
+			} else {
+				do {
+
+				} while (opcion != 0);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-	    
-		
+
 	}
-	
+
 	private String getClientNameFromSocket() throws IOException {
-        // Obtenemos el flujo de entrada del socket para leer el nombre del cliente.
-        BufferedReader input = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
-        return input.readLine(); // Suponiendo que el cliente envía su nombre como una línea de texto.
-    }
-	
-	public static String[] splitByComma(String input) {
-        return input.split(",");
-    }
-	
-	private void enviarMensajeCliente(String mensaje) {
-	    try {
-	        // Obtenemos el OutputStream del socket cliente
-	        PrintWriter out = new PrintWriter(clienteSocket.getOutputStream(), true);
-	        // Enviamos el mensaje al cliente
-	        out.println(mensaje);
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
+		// Obtenemos el flujo de entrada del socket para leer el nombre del cliente.
+		BufferedReader input = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+		return input.readLine(); // Suponiendo que el cliente envía su nombre como una línea de texto.
 	}
-	
-	
+
+	public static String[] splitByComma(String input) {
+		return input.split(",");
+	}
+
+	private void enviarMensajeCliente(String mensaje) {
+		try {
+			// Obtenemos el OutputStream del socket cliente
+			PrintWriter out = new PrintWriter(clienteSocket.getOutputStream(), true);
+			// Enviamos el mensaje al cliente
+			out.println(mensaje);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
