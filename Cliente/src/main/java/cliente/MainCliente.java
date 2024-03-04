c6146e48eda24022572337236a85a625e9aee63f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-04 12:45:37 +0100

    MainCliente.java CHANGES

--- Cliente/src/main/java/cliente/MainCliente.java
+++ Cliente/src/main/java/cliente/MainCliente.java
@@ -5,6 +5,9 @@ public class MainCliente {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Conexion conexion = new Conexion();
+		
+		conexion.conexion();
 	}
 
 }
