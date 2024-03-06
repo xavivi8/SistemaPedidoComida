f4f64a16e4375922083207326ec5726b20b56e4f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-06 20:53:07 +0100

    Funciones.java ADD cifrarContraseñaConMd5

--- Cliente/src/main/java/cliente/Funciones.java
+++ Cliente/src/main/java/cliente/Funciones.java
@@ -1,14 +1,36 @@
 package cliente;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Funciones {
 
-	private static void enviarMensaje(Socket socket, String mensaje) throws IOException {
-		PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-		writer.println(mensaje);
+	public static String cifrarContraseñaConMd5(String laContraseña) {
+		/*
+		 * Nota del alumno MessageDigest: La clase MessageDigest en Java es una parte
+		 * importante de la biblioteca de -seguridad que se utiliza para calcular
+		 * resúmenes criptográficos (hash) de datos, -como contraseñas o mensajes.
+		 * Proporciona la funcionalidad para aplicar algoritmos -de hash criptográficos,
+		 * como MD5, SHA-1, SHA-256, y otros.
+		 * 
+		 * .digest:en Java se utiliza en combinación con la clase MessageDigest para
+		 * calcular un resumen (hash) de los datos proporcionados.
+		 * 
+		 */
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] bytes = md.digest(laContraseña.getBytes());
 
+			StringBuilder sb = new StringBuilder();
+			for (byte b : bytes) {// recorre el array que contiene el hash de MD5
+				sb.append(String.format("%02x", b));// combierte a hexadecimnal de dos caracteres
+			}
+
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
