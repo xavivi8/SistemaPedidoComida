fd691601f8ab50878df7d34449d31e5d57db0239
xavivi8 <jmartinlunasescobar@gmail.com>
2024-03-07 19:06:17 +0100

    Funciones.java ADD COMMENTS

--- Servidor/src/main/java/servidor/Funciones.java
+++ Servidor/src/main/java/servidor/Funciones.java
@@ -1,7 +1,16 @@
 package servidor;
 
+/**
+ * Objetivo: Proporciona funciones auxiliares para el servidor. 
+ * autor: Francisco Javier Martín-Lunas Escobar
+ * fecha: 03/03/2024	
+ */
 public class Funciones {
-
+	
+	/**
+	 * Genera el menú para administradores.
+	 * @return el menú para administradores
+	 */
 	public static String menuUsuario() {
 		return "===== Pedir comida ===== "
 				+ "\n Ver comidas disponibles 1) "
@@ -9,6 +18,10 @@ public class Funciones {
 				+ "\n Terminar 0)";
 	}
 	
+	/**
+	 * Genera el menú para administradores.
+	 * @return el menú para administradores
+	 */
 	public static String menuAdmin() {
 		return "===== Menú administración ===== "
 				+ "\n Rellenar comidas 1)"
@@ -16,6 +29,10 @@ public class Funciones {
 				+ "\n Terminar 0)";
 	}
 	
+	/**
+	 * Genera el menú para rellenar comidas.
+	 * @return el menú para rellenar comidas
+	 */
 	public static String menuRellenarComida() {
 		return "===== Menú rellenar comidas ====="
 				+ "\n Ver comidas 1)"
@@ -24,5 +41,14 @@ public class Funciones {
 				+ "\n Quitar comida 4)"
 				+ "\n Finalizar rellenado 5)"
 				+ "\n Terminar 0)";
+	}
+	
+	/**
+	 * Divide una cadena utilizando como delimitador la coma (,).
+	 * @param input la cadena a dividir
+	 * @return un array de Strings que contiene las subcadenas resultantes
+	 */
+	public static String[] splitByComma(String input) {
+		return input.split(",");
 	}
 }
