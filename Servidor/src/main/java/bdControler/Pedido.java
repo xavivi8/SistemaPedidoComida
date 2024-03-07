40dea20715d534909c3afaf7a5d9a8e67c80ebba
xavivi8 <jmartinlunasescobar@gmail.com>
2024-03-07 20:11:23 +0100

    Pedido.java CREATE

---
+++ Servidor/src/main/java/bdControler/Pedido.java
@@ -0,0 +1,72 @@
+package bdControler;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private int idPedido;
+
+    @Column(name = "usuario")
+    private String usuario;
+
+    @Column(name = "comida")
+    private String comida;
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    public Pedido() {
+    }
+
+    public Pedido(String usuario, String comida, int cantidad) {
+        this.usuario = usuario;
+        this.comida = comida;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getComida() {
+        return comida;
+    }
+
+    public void setComida(String comida) {
+        this.comida = comida;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
