e8127d7d75479bafca45b32ed2f894cf043cac26
xavivi8 <jmartinlunasescobar@gmail.com>
2024-03-07 19:05:16 +0100

    Comida.java ADD COMMENT

--- Servidor/src/main/java/bdControler/Comida.java
+++ Servidor/src/main/java/bdControler/Comida.java
@@ -9,6 +9,11 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 
+/**
+ * Objetivo: Representa la entidad de comida en la base de datos. 
+ * autor: Francisco Javier Martín-Lunas Escobar
+ * fecha: 03/03/2024	
+ */
 @Entity
 @Table(name = "comida")
 public class Comida {
@@ -29,40 +34,81 @@ public class Comida {
 		
 	}
 	
-	// Constructor con todos los campos
+	/**
+	 * Constructor de la clase Comida que inicializa todos los atributos.
+	 * 
+	 * @param idComida el identificador de la comida
+	 * @param nombre el nombre de la comida
+	 * @param cantidad la cantidad de la comida
+	 */
 	public Comida(int idComida, String nombre, int cantidad) {
 		this.idComida = idComida;
 		this.nombre = nombre;
 		this.cantidad = cantidad;
 	}
 
-	// Constructor sin el campo idComida
+	/**
+	 * Constructor de la clase Comida que inicializa los atributos nombre y cantidad.
+	 * 
+	 * @param nombre el nombre de la comida
+	 * @param cantidad la cantidad de la comida
+	 */
 	public Comida(String nombre, int cantidad) {
 		this.nombre = nombre;
 		this.cantidad = cantidad;
 	}
 
 	// Getters y Setters
+	/**
+	 * Devuelve el identificador de la comida.
+	 * 
+	 * @return el identificador de la comida
+	 */
 	public int getIdComida() {
 		return idComida;
 	}
 
+	/**
+	 * Establece el identificador de la comida.
+	 * 
+	 * @param idComida el identificador de la comida
+	 */
 	public void setIdComida(int idComida) {
 		this.idComida = idComida;
 	}
-
+	
+	/**
+	 * Devuelve el nombre de la comida.
+	 * 
+	 * @return el nombre de la comida
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-
+	
+	/**
+	 * Establece el nombre de la comida.
+	 * 
+	 * @param nombre el nombre de la comida
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	
+	/**
+	 * Devuelve la cantidad de la comida.
+	 * 
+	 * @return la cantidad de la comida
+	 */
 	public int getCantidad() {
 		return cantidad;
 	}
-
+	
+	/**
+	 * Establece la cantidad de la comida.
+	 * 
+	 * @param cantidad la cantidad de la comida
+	 */
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
