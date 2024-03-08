e3b765b08a4af00a3d6279c3e7baaa3fdc1ab3f7
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-08 09:13:21 +0100

    MainCliente.java ADD COMMENTS

--- Cliente/src/main/java/cliente/MainCliente.java
+++ Cliente/src/main/java/cliente/MainCliente.java
@@ -1,12 +1,21 @@
 package cliente;
 
+/**
+ * La clase MainCliente es la clase principal que contiene el método main para ejecutar la aplicación del cliente.
+ */
 public class MainCliente {
 
+	/**
+	 * El método main es el punto de entrada de la aplicación del cliente.
+	 * 
+	 * @param args Los argumentos de la línea de comandos (no utilizados en este caso).
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// Se crea una instancia de la clase Conexion para establecer la conexión.
 		Conexion conexion = new Conexion();
 		
+		// Se llama al método conexion para establecer la conexión.
 		conexion.conexion();
 	}
 
